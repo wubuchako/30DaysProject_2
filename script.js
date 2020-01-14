797dd49cb2fa045672ef73ab51e4d9aa6657f31c
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-01-14 15:48:49 +0900

    creating portfolio page

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+$(window).on("load", function(){
+    $(".loader .inner").fadeOut(500, function(){
+        $(".loader").fadeOut(750);
+    });
+
+
+    $(".items").isotope({
+        filter:selector,
+        animationOptions: {
+            duration: 1500,
+            easing:'linear',
+            queue: false
+        }
+    });
+})
+
+
+
+
+
+
 $(document).ready(function(){
     $('#slides').superslides({
         animation: 'fade',
@@ -65,13 +86,59 @@ $(document).ready(function(){
 
     $("[data-fancybox]").fancybox();
 
-    $(".items").isotope({
-        filter:'*',
-        animationOptions: {
-            duration: 1500,
-            easing:'linear',
-            queue: false
+
+    $("#filters a").click(function() {
+        $("#filsters.current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter:selector,
+            animationOptions: {
+                duration: 1500,
+                easing:'linear',
+                queue: false
+            }
+        });
+
+        return false;
+
+    });
+
+
+
+    $("#navigation li a").click(function(e){
+           e.preventDefault();
+
+           var targetElement = $(this).attr("href");
+           var targetPosition = $(targetElement).offset().top;
+           $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+    });
+
+
+
+
+
+
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        var body = $("body");
+
+        if($(window).scrollTop() >= navTop){
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
         }
-    })
+        else {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
+    }
 
 });
